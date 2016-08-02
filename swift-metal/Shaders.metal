07e8a8390cd8fd49070023ee1e7a7785b2fd43be
Ryan Medeiros <ryan.medeiros@activision.com>
2016-08-01 19:10:27 -0700

    I Made a Triangle
    I wanted it colored. So I did that too.

---
+++ swift-metal/Shaders.metal
@@ -0,0 +1,32 @@
+//
+//  Shaders.metal
+//  swift-metal
+//
+//  Created by Ryan on 8/1/16.
+//  Copyright © 2016 DWishR. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexOutput
+{
+    float4 position [[ position ]];
+    float4 color;
+};
+
+vertex VertexOutput basic_vertex(
+        const device packed_float3* vertex_array [[ buffer(0) ]],
+        const device packed_float3* color_array [[ buffer(1) ]],
+        uint vid [[ vertex_id ]])
+{
+    VertexOutput vo;
+    vo.position = float4(vertex_array[vid], 1);
+    vo.color = half4(color_array[vid][0], color_array[vid][1], color_array[vid][2],1);
+    return vo;
+}
+
+fragment float4 basic_fragment( VertexOutput vert [[stage_in]] )
+{
+    return vert.color;
+}
--- swift-metal/ViewController.swift
+++ swift-metal/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import Metal
+import QuartzCore
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    let vertexData:[Float] = [
+        0, 0.5, 0,
+        -0.66667, -0.66667, 0,
+        0.66667, -0.66667, 0
+    ]
+    
+    let colorData:[Float] = [
+        1,0,0,
+        0,1,0,
+        0,0,1
+    ]
+    
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    
+    var vertexBuffer: MTLBuffer! = nil
+    var colorBuffer: MTLBuffer! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    
+    
+    var timer: CADisplayLink! = nil
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .BGRA8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let vSize = vertexData.count * sizeof(Float)
+        vertexBuffer = device.newBufferWithBytes(vertexData, length: vSize, options:MTLResourceOptions())
+        
+        let cSize = colorData.count * sizeof(Float)
+        colorBuffer = device.newBufferWithBytes(colorData, length: cSize, options: MTLResourceOptions())
+        
+        let defaultLibrary = device.newDefaultLibrary()
+        let vertexProgram = defaultLibrary!.newFunctionWithName("basic_vertex")
+        let fragmentProgram = defaultLibrary!.newFunctionWithName("basic_fragment")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm
+        pipelineState = try! device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor)
+        
+        commandQueue = device.newCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: #selector(tick))
+        timer.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func render()
+    {
+        let drawable = metalLayer.nextDrawable()
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable!.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .Clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.25, 0.25, 0.25, 1)
+        
+        let commandBuffer = commandQueue.commandBuffer()
+        
+        let renderEncoder = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, atIndex: 0)
+        renderEncoder.setVertexBuffer(colorBuffer, offset: 0, atIndex: 1)
+        renderEncoder.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 3)
+        renderEncoder.endEncoding()
+        
+        commandBuffer.presentDrawable(drawable!)
+        commandBuffer.commit()
+    }
+    
+    func tick()
+    {
+        autoreleasepool
+        {
+            render()
+        }
+    }
 }
 
